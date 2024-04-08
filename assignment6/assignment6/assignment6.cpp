dc8d0efb35351eea4352532aa971882dfa8180b2
Asim Kara <akara4314@conestogac.on.ca>
2024-04-08 16:11:46 -0400

    Created a new branch

--- assignment6/assignment6/assignment6.cpp
+++ assignment6/assignment6/assignment6.cpp
@@ -3,17 +3,10 @@
 
 int main(void)
 {
-	char something[32] = "RCS for SEF";
+	char something[32] = "Version control for SEF";
+	char something[32] = "Conflict attempt";
 
 	printf("%s", something);
-	bool check = false;
-
-	int size = strlen(something);
-
-	if (size != NULL)
-	{
-		printf("%s", something);
-	}
-
+	
 	return 0;
 }
